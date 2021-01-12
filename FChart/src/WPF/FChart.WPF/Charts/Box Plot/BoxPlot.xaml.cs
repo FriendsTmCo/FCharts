ecea244380daf7d9df3ac6a4606ed8c02228835c
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-12 21:44:38 +0330

    insert Charts

---
+++ FChart/src/WPF/FChart.WPF/Charts/Box Plot/BoxPlot.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FChart.WPF.Charts.Box_Plot
+{
+    /// <summary>
+    /// Interaction logic for BoxPlot.xaml
+    /// </summary>
+    public partial class BoxPlot : UserControl
+    {
+        public BoxPlot()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FChart/src/WPF/FChart.WPF/Charts/Histo Gram/HistoGramChart.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FChart.WPF.Charts.Histo_Gram
+{
+    /// <summary>
+    /// Interaction logic for HistoGramChart.xaml
+    /// </summary>
+    public partial class HistoGramChart : UserControl
+    {
+        public HistoGramChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
