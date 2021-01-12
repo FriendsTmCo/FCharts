b262fa987ce870718f85d5619f09c1b7998d7a10
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-12 21:49:26 +0330

    Insert Other Charts

---
+++ FChart/src/WPF/FChart.WPF/Charts/Map_Based Plots/MapBasedPlots.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FChart.WPF.Charts.Map_Based_Plots
+{
+    /// <summary>
+    /// Interaction logic for MapBasedPlots.xaml
+    /// </summary>
+    public partial class MapBasedPlots : UserControl
+    {
+        public MapBasedPlots()
+        {
+            InitializeComponent();
+        }
+    }
+}
